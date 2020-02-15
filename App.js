5a0eb3aa84a46ffed348fe0b9a367642a089a2e9
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-15 01:41:38 +0100

    Basic layout of buttons

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// computer choice generation
+// user choice
+// compare computer choice to user choice
+// the first one who gets to 3 points wins
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      moves: ['rock', 'paper', 'scissors'],
-      points: 0,
+      computerMoves: ['rock', 'paper', 'scissors'],
+      userMove: '',
+      userPoints: 0,
+      computerPoints: 0,
     }
+  }
+
+  initializeGame() {
+
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text onPress={() => this.setState({points: 1})} >Rock Paper Scissors - React Native</Text>
-        <Text>
-          {this.state.points}
-        </Text>
+        <View style={styles.choices}>
+          <TouchableOpacity style={styles.choice} onPress={() => this.setState({ userMove: 'rock' })} >
+            <Icon name="hand-rock-o" size={80} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.choice} onPress={() => this.setState({ userMove: 'paper' })} >
+            <Icon name="paper-plane" size={80} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.choice} onPress={() => this.setState({ userMove: 'scissors' })} >
+            <Icon name="scissors" size={80} />
+          </TouchableOpacity>
+        </View>
+
+        <Text> { this.state.userMove } </Text>
       </View>
     );
   }
@@ -28,6 +51,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  choice: {
+    padding: 20,
+  },
+  choices: {
+    flexDirection: 'row'
+  }
 });
